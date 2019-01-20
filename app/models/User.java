1d7935b6f18d84ce1606a3fd52cb74e378ef5738
Rodrigo Zanini <43685688+r-zanini@users.noreply.github.com>
2019-01-19 20:38:19 -0500

    Create User

---
+++ app/models/User.java
@@ -0,0 +1,16 @@
+package models;
+
+import java.util.Date;
+
+public class User {
+	private String userName;
+	private String password;
+	private String address;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private Date dob;
+	private String phoneNumber;
+	private Byte[] picture;
+	private String parentUser;
+}
